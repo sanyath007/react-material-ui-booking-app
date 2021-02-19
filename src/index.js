fd56d4642ab839baa39c849b96d45bb8d9565cc1
sanyath007 <sanyath007@gmail.com>
2021-02-19 12:07:38 +0700

    Set default locale of MuiPickerUtilsProvider to moment/locale/th

--- src/index.js
+++ src/index.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import moment from 'moment';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import { MuiPickersUtilsProvider } from '@material-ui/pickers';
 import MomentUtils from '@date-io/moment';
-
+import 'moment/locale/th';
 import App from './App';
 import store from './redux/store';
 import * as serviceWorker from './serviceWorker';
 
+moment.locale('th');
+
 ReactDOM.render((
   <Provider store={store}>
     <BrowserRouter>
-      <MuiPickersUtilsProvider utils={MomentUtils}>
+      <MuiPickersUtilsProvider utils={MomentUtils} libInstance={moment}>
         <App />
       </MuiPickersUtilsProvider>
     </BrowserRouter>
