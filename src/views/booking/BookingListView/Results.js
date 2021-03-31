e63ae46cd39cdd12d6e30c3e7af8e441971ced8f
sanyath007 <sanyath007@gmail.com>
2021-03-31 14:49:06 +0700

    Style star icon color in booking/BookingListView/Results

--- src/views/booking/BookingListView/Results.js
+++ src/views/booking/BookingListView/Results.js
@@ -161,7 +161,7 @@ const Results = ({
                       >
                         {`${booking.an?.patient?.pname}${booking.an?.patient?.fname} ${booking.an?.patient?.lname}`}
                       </Typography>
-                      {booking.is_officer && <StarIcon />}
+                      {booking.is_officer && <StarIcon style={{ fill: 'red' }} />}
                     </Box>
                   </TableCell>
                   <TableCell align="center">
