4a467aa915e6a01c2dca7369d84f3f85b98a3e99
sanyath007 <sanyath007@gmail.com>
2022-03-09 10:39:07 +0700

    Reflactor ipSlice redux by change all action creators to async thunk actions with coordinated extraReducers and remove unused reducers

--- src/redux/ip/index.js
+++ src/redux/ip/index.js
@@ -1,6 +1,40 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import errorHandler from 'src/utils/responseErrorHandler';
 import axios from 'axios';
 import api from '../../api';
+
+// Actions
+export const fetchAll = createAsyncThunk('ip/fetchAll', async ({ qs = '' }) => {
+  try {
+    const res = await api.get(`/ips${qs}`);
+
+    return res.data;
+  } catch (error) {
+    errorHandler(error);
+  }
+});
+
+export const fetchAllWithPage = createAsyncThunk('ip/fetchAll', async ({ url }) => {
+  try {
+    const res = await axios.get(url);
+
+    return res.data;
+  } catch (error) {
+    errorHandler(error);
+  }
+});
+
+export const fetchIpByAn = createAsyncThunk('ip/fetchByAn', async ({ an = '' }) => {
+  try {
+    const res = await api.get(`/ips/${an}`);
+
+    return res.data;
+  } catch (error) {
+    errorHandler(error);
+  }
+});
+
+// TODO: add CRUD action of room
 
 export const ipSlice = createSlice({
   name: 'ip',
@@ -8,51 +42,39 @@ export const ipSlice = createSlice({
     ip: {},
     ips: [],
     pager: null,
+    loading: false,
+    error: ''
   },
-  reducers: {
-    fetchAllSuccess: (state, action) => {
+  reducers: {},
+  extraReducers: {
+    [fetchAll.pending]: (state) => {
+      state.loading = true;
+      state.error = '';
+    },
+    [fetchAll.fulfilled]: (state, action) => {
       state.ips = action.payload.items;
       state.pager = action.payload.pager;
+      state.loading = false;
+      state.error = '';
     },
-    fetchIpByAnSuccess: (state, action) => {
+    [fetchAll.rejected]: (state, action) => {
+      state.loading = false;
+      state.error = action.error;
+    },
+    [fetchIpByAn.pending]: (state) => {
+      state.loading = true;
+      state.error = '';
+    },
+    [fetchIpByAn.fulfilled]: (state, action) => {
       state.ip = action.payload;
+      state.loading = false;
+      state.error = '';
+    },
+    [fetchIpByAn.rejected]: (state, action) => {
+      state.loading = false;
+      state.error = action.error;
     },
   }
 });
 
 export default ipSlice.reducer;
-
-// Actions
-const { fetchAllSuccess, fetchIpByAnSuccess } = ipSlice.actions;
-
-export const fetchIpAll = (qs = '') => async (dispatch) => {
-  try {
-    const res = await api.get(`/ips${qs}`);
-
-    return dispatch(fetchAllSuccess(res.data));
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-export const fetchIpAllWithPage = (url) => async (dispatch) => {
-  try {
-    const res = await axios.get(url);
-
-    return dispatch(fetchAllSuccess(res.data));
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-export const fetchIpByAn = (an) => async (dispatch) => {
-  try {
-    const res = await api.get(`/ips/${an}`);
-
-    return dispatch(fetchIpByAnSuccess(res.data));
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-// TODO: add CRUD action of room
