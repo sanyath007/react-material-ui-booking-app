9d6d7949c1c894f5bf97ffe82a7fdd9e03b1df26
sanyath007 <sanyath007@gmail.com>
2022-03-14 14:18:05 +0700

    Reflactor rendering amenities data in AmenityLists by using map array method

--- src/views/room/RoomDetail/AmenityLists/index.js
+++ src/views/room/RoomDetail/AmenityLists/index.js
@@ -7,54 +7,20 @@ import {
 } from '@material-ui/core';
 
 const AmenityLists = ({ amenities }) => {
-  console.log(amenities);
-
   return (
     <Grid container spacing={2}>
-      <Grid item xs={6} md={3}>
-        <Icon>bathtub</Icon>
-        <span style={{ marginLeft: '10px' }}>ห้องน้ำ</span>
-      </Grid>
-      <Grid item xs={6} md={3}>
-        <Icon>shower</Icon>
-        <span style={{ marginLeft: '10px' }}>เครื่องทำน้ำอุ่น</span>
-      </Grid>
-      <Grid item xs={6} md={3}>
-        <Icon>bed</Icon>
-        <span style={{ marginLeft: '10px' }}>เตียงไฟฟ้า</span>
-      </Grid>
-      <Grid item xs={6} md={3}>
-        <Icon>tv</Icon>
-        <span style={{ marginLeft: '10px' }}>โทรทัศน์</span>
-      </Grid>
-      <Grid item xs={6} md={3}>
-        <Icon>air</Icon>
-        <span style={{ marginLeft: '10px' }}>เครื่องปรับอากาศ</span>
-      </Grid>
-      <Grid item xs={6} md={3}>
-        <Icon>kitchen</Icon>
-        <span style={{ marginLeft: '10px' }}>ตู้เย็น</span>
-      </Grid>
-      <Grid item xs={6} md={3}>
-        <Icon>microwave</Icon>
-        <span style={{ marginLeft: '10px' }}>ไมโครเวฟ</span>
-      </Grid>
-      <Grid item xs={6} md={3}>
-        <Icon>checkroom</Icon>
-        <span style={{ marginLeft: '10px' }}>ตู้เสื้อผ้า</span>
-      </Grid>
-      <Grid item xs={6} md={3}>
-        <Icon>chair</Icon>
-        <span style={{ marginLeft: '10px' }}>โซฟา</span>
-      </Grid>
-      <Grid item xs={6} md={3}>
-        <Icon>table_bar</Icon>
-        <span style={{ marginLeft: '10px' }}>โต๊ะอาหาร</span>
-      </Grid>
-      <Grid item xs={6} md={3}>
-        <Icon>wifi</Icon>
-        <span style={{ marginLeft: '10px' }}>Wifi</span>
-      </Grid>
+      {amenities && amenities.map((room) => {
+        const { amenity } = room;
+
+        return (
+          <Grid item xs={6} md={3} key={amenity[0].amenity_id}>
+            <Icon>{amenity[0].amenity_icon}</Icon>
+            <span style={{ marginLeft: '10px' }}>
+              {amenity[0].amenity_desc}
+            </span>
+          </Grid>
+        );
+      })}
     </Grid>
   );
 };
